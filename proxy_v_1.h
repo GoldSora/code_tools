ba7422c672bf5b9d1eb7c6b4ab8d29e12718c1cb
GoldSora <36560377+GoldSora@users.noreply.github.com>
2022-03-18 10:43:26 +0800

    Create proxy_v_1.h

---
+++ proxy_v_1.h
@@ -0,0 +1,12 @@
+#ifdef __cplusplus
+extern "C"
+#endif
+
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void start_task();
